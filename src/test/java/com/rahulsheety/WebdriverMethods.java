a4516a911e2743842c3e6928b47f605cfe3d3be6
rshekar005 <rshekar005@gmail.com>
2020-09-04 11:30:38 +0530

    Last commit for Naveen Automation Labs

---
+++ src/test/java/com/rahulsheety/WebdriverMethods.java
@@ -0,0 +1,29 @@
+package com.rahulsheety;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebdriverMethods {
+	
+	static WebDriver driver;
+	public static void main(String args[])
+	{
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Title of the page is "+driver.getTitle());
+		System.out.println("Current URL is "+driver.getCurrentUrl());
+		/*System.out.println("Page source is "+driver.getPageSource());*/
+		
+		driver.get("https://www.yahoo.com");
+		System.out.println("Title of the page is "+driver.getTitle());
+		System.out.println("Current URL is "+driver.getCurrentUrl());
+		driver.navigate().back();
+	
+		
+	}
+
+}

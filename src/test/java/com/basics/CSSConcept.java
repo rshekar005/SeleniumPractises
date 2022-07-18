9c5a0dfee920e5817cc9fa1edb9af2939933efe7
rshekar005 <rshekar005@gmail.com>
2022-07-18 13:05:20 +0530

    Added CSSConcept

--- src/test/java/com/basics/CSSConcept.java
+++ src/test/java/com/basics/CSSConcept.java
@@ -2,25 +2,42 @@ package com.basics;
 /**
  * 
  * @author rajasekhar
- *	Using id == use # symbol 
+ *1. Using id == use # symbol
  *              syntax: htmltag#id or #id
  *              Example: ul#categories  or #categories
  *              
- *  Using class  == use . symbol  
+ * 2. Using class  == use . symbol
  *  			syntax: htmltag.classname, htmltag.c1.c2.c3 etc / .classname.classname2.classname3
  *              Example; input.form-control.private-form__control.login-email
  *                       .form-control.private-form__control.login-email
  *                       
- *  using attribute == //tagname[attribute='value']
- *  			Example: //div[id='test']
+ *  using attribute == tagname[attribute='value']
+ *  			Example: div[id='test']
  *  
- *  using multiple attribute == //tagname[attribute='value'][attribute2='value'] 
+ *  using multiple attribute == tagname[attribute='value'][attribute2='value']
  *  contains == * symbol  --> ex: input[id*='user']
  *  
  *  Starts-with == ^ symbol  --> ex: input[id^='user']
  *  Ends with == $ symbol  --> input[id$='name']
  *  
- *  parent>child
+ *  parent>childtag
+ *
+ *  ul#categories>li  -- Here it will point all the child tags which are with li under ul tag.
+ *
+ * first-of-type in css selector
+ * ul#categories>li:first-of-type   -- Here it will select only first element which present in li tag under parent ul tag
+ *
+ * last-of-type in css selector
+ * ul#categories>li:last-of-type  -- Here it will select only last element which present in li tag under parent ul tag.
+ *
+ * nth-of-type in css-selector
+ * ul#categories>li:nth-of-type(3)   -- Here it will select only 3rd element which present in li tag under parent ul tag.
+ *
+ * Sibling:
+ * xpath for sibling: //div[@class='private-form__control-wrapper']//following-sibling::div -- It will point to siling point which present in parent div tag
+ * CSS Sibling:  div.private-form__control-wrapper+div  -- It will point sibling tags
+ *
+ *
  */
 public class CSSConcept {
 
